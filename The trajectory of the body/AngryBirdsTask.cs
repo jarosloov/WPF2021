f9c61df8c9b387bbacc03bb7848ded063ad5c789
Yaroslav <60881838+jarosloov@users.noreply.github.com>
2021-02-12 10:49:15 +0300

    Траектория движения тела

---
+++ The trajectory of the body/AngryBirdsTask.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AngryBirds
+{
+	public static class AngryBirdsTask
+	{
+		public static double FindSightAngle(double v, double distance)
+		{
+			double g = 9.8;
+			return Math.Asin((distance * g) / (v * v)) / 2;
+		}
+	}
+}
