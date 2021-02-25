3342eb5f9a7a4c5795ed3a1ce277b987e0ee00b1
Yaroslav <60881838+jarosloov@users.noreply.github.com>
2021-02-25 22:59:34 +0500

    Test

---
+++ TestCodeWPF/SayHello.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+
+namespace FirstCodeWPF
+{
+    class SayHello
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Window win = new Window();
+            Application app = new Application();
+
+            win.Title = "Jarosloov";
+            win.Show();
+            app.Run();
+        }
+    }
+}
