6204931bcd8d27031bff8931195db40f2ef125da
Jarosloov <jarosloov@yandex.ru>
2021-02-28 12:16:08 +0500

    Движение тела брошенное под углом к горизонту

--- Movement of a body thrown at an angle to the horizon/Program.cs
+++ Movement of a body thrown at an angle to the horizon/Program.cs
@@ -18,12 +18,12 @@ namespace ConsoleApp1
             double speed = Convert.ToDouble(Console.ReadLine());
             double angle = Convert.ToDouble(Console.ReadLine());
             double time = (2 * speed * Math.Sin(angle * Math.PI / 180) / G);
-            // Время и растояние полёта 
+            // Время и растояние полёта   
             Console.WriteLine("Time of fight: " + Math.Round((2 * speed *
                 Math.Sin(angle * Math.PI / 180) / G)), 3);
             Console.WriteLine("Length of fight: " + Math.Round((speed * speed *
                 Math.Sin(2*angle * Math.PI / 180)) / G), 3);
-            // запись
+            // запись   
             for (double t = 0; t <= time ; t += 0.01)
                 coordinates.Add(Math.Round((speed * t * Math.Cos(angle * Math.PI / 180)), 3),
                     Math.Round((speed * t * Math.Sin(angle * Math.PI / 180) - G * t * t / 2), 3)) ;   
