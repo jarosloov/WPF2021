97611b5eaaf4db04a0afd65e9362ae93465cef2c
Jarosloov <jarosloov@yandex.ru>
2021-02-28 12:12:03 +0500

    Движенние тела брошенного под углом к горизонту

---
+++ Movement of a body thrown at an angle to the horizon/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<double, double> coordinates = new Dictionary<double, double>();
+            const double G = 9.80665;
+            double speed = Convert.ToDouble(Console.ReadLine());
+            double angle = Convert.ToDouble(Console.ReadLine());
+            double time = (2 * speed * Math.Sin(angle * Math.PI / 180) / G);
+            // Время и растояние полёта 
+            Console.WriteLine("Time of fight: " + Math.Round((2 * speed *
+                Math.Sin(angle * Math.PI / 180) / G)), 3);
+            Console.WriteLine("Length of fight: " + Math.Round((speed * speed *
+                Math.Sin(2*angle * Math.PI / 180)) / G), 3);
+            // запись
+            for (double t = 0; t <= time ; t += 0.01)
+                coordinates.Add(Math.Round((speed * t * Math.Cos(angle * Math.PI / 180)), 3),
+                    Math.Round((speed * t * Math.Sin(angle * Math.PI / 180) - G * t * t / 2), 3)) ;   
+            // вывод
+            foreach (var i in coordinates)
+                Console.WriteLine(i);
+        }
+    }
+}
--- TestCodeWPF/SayHello.cs
+++ TestCodeWPF/SayHello.cs
@@ -1,19 +1,35 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Input;
 
 namespace FirstCodeWPF
 {
-    class SayHello
+    class SayHello : Application
     {
         [STAThread]
         public static void Main()
         {
-            Window win = new Window();
-            Application app = new Application();
-
-            win.Title = "JAROSLOOV";
-            win.Show();
+            SayHello app = new SayHello();
             app.Run();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Window win = new Window();
+            win.Title = "Inherit the App";
+            win.Show();
+        }
+
+        protected override void OnSessionEnding(SessionEndingCancelEventArgs e)
+        {
+            base.OnSessionEnding(e);
+            MessageBoxResult result = MessageBox.Show("Do you want to save your data?", MainWindow.Title,
+                                                      MessageBoxButton.YesNoCancel, MessageBoxImage.Question,
+                                                      MessageBoxResult.Yes);
+
+
         }
     }
 }
