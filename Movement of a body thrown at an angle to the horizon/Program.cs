2671df8299197824f706b6a89c3c3ff22950d2e8
Jarosloov <jarosloov@yandex.ru>
2021-03-03 21:16:37 +0500

    Опубликовать проект для закона движения для полета тела по параболической траектории.

--- Movement of a body thrown at an angle to the horizon/Program.cs
+++ Movement of a body thrown at an angle to the horizon/Program.cs
@@ -1,14 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    
-    
     class Program
     {
         static void Main(string[] args)
