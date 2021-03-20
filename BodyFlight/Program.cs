6c012b6f8e71f2797249ec1ea2daa2acfacd58ae
Jarosloov <jarosloov@yandex.ru>
2021-03-20 22:22:17 +0500

    Добавление "Интерфейса"

--- BodyFlight/Program.cs
+++ BodyFlight/Program.cs
@@ -18,15 +18,16 @@ namespace BodyFlight
         private double time;
         private double angle;
 
-        private double fullFlightTime;
-        private double maximumFlightAltitude;
-        private double flightLength;
+        bool statusTXT = false;
+        bool statusExcel = false;
+
+        //private double fullFlightTime;
+        //private double maximumFlightAltitude;
+        //private double flightLength;
 
         Dictionary<double, double> coordinates = new Dictionary<double, double>();
 
         private const double G = 9.80665;
-
-
 
         /// <summary>
         /// Скорость, время полета, угол наклона 
@@ -69,27 +70,42 @@ namespace BodyFlight
             speed = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Введите угол наклона: ");
             angle = Convert.ToDouble(Console.ReadLine());
+            time = 2 * speed * Math.Sin(angle * Math.PI / 180) / G;
         }
 
         private void Menu()
         {
             Console.Clear();
-            Console.WriteLine("Вызволось меню:");
-            Console.WriteLine("Нажмите [I] чтобы вызвать информацию");
+            Console.WriteLine("############  Meню  ############");
+            Console.WriteLine("[I] - информаця");
+            if(statusExcel)
+                Console.WriteLine("[E] - запись в .excel \t [записано]");
+            else
+                Console.WriteLine("[E] - запись в .excel \t [не записано]");
+            if(statusTXT)
+                Console.WriteLine("[T] - запись в .txt  \t [записано]");
+            else
+                Console.WriteLine("[T] - запись в .txt  \t [не записано]");
+            Console.WriteLine("[Esc] - выход");
             ConsoleKeyInfo key = Console.ReadKey();
-            //switch (key.Key)
-            //{
-            //    case ConsoleKey.I:
-            //        OutputInformation();
-            //        break;
-
-            //    default:
-            //        Console.WriteLine("Default case");
-            //        break;
-            //}
-            if (key.Key == ConsoleKey.I)
-                OutputInformation();
-            Console.Clear();
+            while (true)
+            {
+                if (key.Key == ConsoleKey.I)
+                    OutputInformation();
+                else if (key.Key == ConsoleKey.E)
+                    OutputToFileExcel();
+                else if (key.Key == ConsoleKey.T)
+                    OutputToFileTXT();
+                else if (key.Key == ConsoleKey.Escape)
+                    break;
+                else if((key.Key != ConsoleKey.I) || (key.Key != ConsoleKey.E) || 
+                    (key.Key != ConsoleKey.T) || (key.Key != ConsoleKey.Escape))
+                     {
+                        Console.WriteLine("Нет такой команды! Повторите.");
+                        key = Console.ReadKey();
+                     }
+                    
+            }
         }
 
         private bool OutputToFileExcel()
@@ -104,8 +120,134 @@ namespace BodyFlight
 
         private void OutputInformation()
         {
-            Console.WriteLine("Вызволось инфо");
-            Menu();
+            Console.Clear();
+            Console.WriteLine("############  Показ информации  ############");
+            Console.WriteLine("[I] - Основная информаця");
+            Console.WriteLine("[С] - Показ всех координат");
+            Console.WriteLine("[T] - Координыты в текущий момент времени");
+            Console.WriteLine("[Esc] - вернуться назад");
+            ConsoleKeyInfo key = Console.ReadKey();
+            while (true)
+            {
+                if (key.Key == ConsoleKey.I)
+                    OutputIndication();
+                else if (key.Key == ConsoleKey.C)
+                    OutputFlightCoordinates();
+                else if (key.Key == ConsoleKey.T)
+                    OutputToFileTXT();
+                else if (key.Key == ConsoleKey.Escape)
+                    Menu();
+                else if ((key.Key != ConsoleKey.I) || (key.Key != ConsoleKey.C) ||
+                    (key.Key != ConsoleKey.T) || (key.Key != ConsoleKey.Escape))
+                {
+                    Console.WriteLine("Нет такой команды! Повторите.");
+                    key = Console.ReadKey();
+                }
+            }
+        }
+
+        private void OutputIndication()
+        {
+            Console.Clear();
+            Console.WriteLine("############  Показ информации  ############");
+            Console.WriteLine("  #########  Основные показатели  ########");
+            Console.WriteLine(" ");
+            Console.WriteLine("[0] - Скорость тела в начальный момент времени:      " +
+                speed);
+            Console.WriteLine("[1] - Угол наглона тела при запуске:     " +
+                angle);
+            Console.WriteLine("[2] - Полное время полета тела:      " +
+                Math.Round(time, 3));
+            Console.WriteLine("[3] - Максимальная высота полета тела:       " +
+                Math.Round((speed * speed * Math.Sin(angle * Math.PI / 180)
+                * Math.Sin(angle * Math.PI / 180)) / 2*G), 3);
+            Console.WriteLine("[4] - Растояние которое пролетело тело:      " +
+                Math.Round((speed * speed * Math.Sin(2 * angle * Math.PI / 180)) / G), 3);
+            Console.WriteLine(" ");
+            Console.WriteLine("------------------------------------------------------");
+            Console.WriteLine(" ");
+            Console.WriteLine("[Esc] - вернуться назад");
+            ConsoleKeyInfo key = Console.ReadKey();
+
+            if (key.Key == ConsoleKey.Escape)
+                OutputInformation();
+            else if (key.Key != ConsoleKey.Escape)
+            {
+                Console.WriteLine("Нет такой команды! Повторите.");
+                key = Console.ReadKey();
+            }
+        }
+
+        private void OutputFlightCoordinates()
+        {
+            Console.Clear();
+            Console.WriteLine("############  Показ информации  ############");
+            Console.WriteLine("  ##########  Координаты полета  #########");
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Введите интрервал времени с которым хотите получать координаты полёта тела:");
+            Console.WriteLine("!Вместо запятых нужно узаказывать точку!");
+            double intervalTime =  Convert.ToDouble(Console.ReadLine());
+            if(intervalTime <= 0)
+            {
+                Console.WriteLine("Вы ввели '0' или  число < 0 . Зачем? Введите число > 0");
+                intervalTime = Convert.ToDouble(Console.ReadLine());
+            }
+            //for (double t = 0; t <= time; t += intervalTime)
+            //    Console.WriteLine("[x]:\t"+ Math.Round((speed * t * Math.Cos(angle * Math.PI / 180)), 3) +'\t' 
+            //        + "[y]:\t" + Math.Round((speed * t * Math.Sin(angle * Math.PI / 180) - G * t * t / 2), 3));
+            Console.WriteLine(" ");
+            Console.WriteLine("------------------------------------------------------");
+            Console.WriteLine(" ");
+            Console.WriteLine("[Esc] - вернуться назад");
+            ConsoleKeyInfo key = Console.ReadKey();
+            if (key.Key == ConsoleKey.Escape)
+                OutputInformation();
+            else if (key.Key != ConsoleKey.Escape)
+            {
+                Console.WriteLine("Нет такой команды! Повторите.");
+                key = Console.ReadKey();
+            }
+        }
+
+        private void OutputCoordinatesAtGivenTime()
+        {
+            Console.Clear();
+            Console.WriteLine("############  Показ информации  ############");
+            Console.WriteLine(" ##  Координаты в данный момент времени  ##");
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Введите время в момент которого вы хотетие узнать положение тела:");
+            double timeMoment = Convert.ToDouble(Console.ReadLine());
+            if(timeMoment > time)
+            {
+                Console.WriteLine("В данный момент времени тело уже приземлилось. Контакт с землей был на растоянии от старта:\t" 
+                    + Math.Round((speed * speed * Math.Sin(2 * angle * Math.PI / 180)) / G), 3);
+            }
+            else if(timeMoment < time)
+            {
+                Console.WriteLine("[x]:\t"
+                    + Math.Round((speed * timeMoment * Math.Cos(angle * Math.PI / 180)), 3));
+                Console.WriteLine("[y]:\t"
+                    + Math.Round((speed * timeMoment * Math.Sin(angle * Math.PI / 180) - G * timeMoment * timeMoment / 2), 3));
+            }
+            else
+            {
+                Console.WriteLine("[x]:\t 0");
+                Console.WriteLine("[y]:\t 0");
+            }    
+            Console.WriteLine(" ");
+            Console.WriteLine("------------------------------------------------------");
+            Console.WriteLine(" ");
+            Console.WriteLine("[Esc] - вернуться назад");
+            ConsoleKeyInfo key = Console.ReadKey();
+            if (key.Key == ConsoleKey.Escape)
+                OutputInformation();
+            else if (key.Key != ConsoleKey.Escape)
+            {
+                Console.WriteLine("Нет такой команды! Повторите.");
+                key = Console.ReadKey();
+            }
         }
     }
 }
