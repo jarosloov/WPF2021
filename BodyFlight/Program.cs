79180fd0fc8135c34c198c0cda308ad32b197454
Jarosloov <jarosloov@yandex.ru>
2021-03-21 18:31:29 +0500

    Добавлена функиция запись в файла .txt

--- BodyFlight/Program.cs
+++ BodyFlight/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
+using Excel = Microsoft.Office.Interop.Excel;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace BodyFlight
 {
@@ -20,10 +22,7 @@ namespace BodyFlight
 
         bool statusTXT = false;
         bool statusExcel = false;
-
-        //private double fullFlightTime;
-        //private double maximumFlightAltitude;
-        //private double flightLength;
+        bool statusInput = true;
 
         Dictionary<double, double> coordinates = new Dictionary<double, double>();
 
@@ -60,62 +59,168 @@ namespace BodyFlight
         /// </summary>
         public BodyFlight()
         {
-            Input();
             Menu();
         }
 
         private void Input()
         {
+            Console.Clear();
             Console.WriteLine("Введите скорость тела: ");
             speed = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Введите угол наклона: ");
             angle = Convert.ToDouble(Console.ReadLine());
             time = 2 * speed * Math.Sin(angle * Math.PI / 180) / G;
+            statusInput = false;
+            Menu();
         }
 
         private void Menu()
         {
-            Console.Clear();
             Console.WriteLine("############  Meню  ############");
-            Console.WriteLine("[I] - информаця");
-            if(statusExcel)
-                Console.WriteLine("[E] - запись в .excel \t [записано]");
+            if(statusInput)
+            {
+                Console.WriteLine("[D] - Ввод данных \t [нет данных]");
+                Console.WriteLine("[I] - информаця \t [нет данных]");
+                if (statusExcel)
+                    Console.WriteLine("[E] - запись в .excel \t [записано][нет данных]");
+                else
+                    Console.WriteLine("[E] - запись в .excel \t [не записано][нет данных]");
+                if (statusTXT)
+                    Console.WriteLine("[T] - запись в .txt  \t [записано][нет данных]");
+                else
+                    Console.WriteLine("[T] - запись в .txt  \t [не записано][нет данных]");
+                Console.WriteLine("[Esc] - выход");
+            }
             else
-                Console.WriteLine("[E] - запись в .excel \t [не записано]");
-            if(statusTXT)
-                Console.WriteLine("[T] - запись в .txt  \t [записано]");
-            else
-                Console.WriteLine("[T] - запись в .txt  \t [не записано]");
-            Console.WriteLine("[Esc] - выход");
+            {
+                Console.WriteLine("[D] - Ввод новых данных \t [есть данные]");
+                Console.WriteLine("[I] - информаця \t [есть данные]");
+                
+                if (statusExcel)
+                    Console.WriteLine("[E] - запись в .excel \t [записано]");
+                else
+                    Console.WriteLine("[E] - запись в .excel \t [не записано]");
+                if (statusTXT)
+                    Console.WriteLine("[T] - запись в .txt  \t [записано]");
+                else
+                    Console.WriteLine("[T] - запись в .txt  \t [не записано]");
+                Console.WriteLine("[Esc] - выход");
+            }
+                
             ConsoleKeyInfo key = Console.ReadKey();
             while (true)
             {
                 if (key.Key == ConsoleKey.I)
                     OutputInformation();
                 else if (key.Key == ConsoleKey.E)
+                {
                     OutputToFileExcel();
+                    Console.Clear();
+                }
                 else if (key.Key == ConsoleKey.T)
+                {
                     OutputToFileTXT();
+                    Console.Clear();
+                }
+                else if(key.Key == ConsoleKey.D)
+                {
+                    Input();
+                    Console.Clear();
+                }
                 else if (key.Key == ConsoleKey.Escape)
-                    break;
-                else if((key.Key != ConsoleKey.I) || (key.Key != ConsoleKey.E) || 
+                    Process.GetCurrentProcess().Kill();
+                else if((key.Key != ConsoleKey.I) || (key.Key != ConsoleKey.D) || (key.Key != ConsoleKey.E) || 
                     (key.Key != ConsoleKey.T) || (key.Key != ConsoleKey.Escape))
                      {
                         Console.WriteLine("Нет такой команды! Повторите.");
                         key = Console.ReadKey();
                      }
-                    
             }
         }
 
-        private bool OutputToFileExcel()
+        private void OutputToFileExcel()
         {
-            return true;
+            Console.WriteLine("####  запись в .excel  ####");
+            Console.WriteLine(" ");
+            Console.WriteLine("##  Находится в разработке ##");
+
+
+
+            Console.WriteLine(" ");
+            Console.WriteLine("------------------------------------------------------");
+            Console.WriteLine(" ");
+            Console.WriteLine("[Esc] - вернуться назад");
+            ConsoleKeyInfo key = Console.ReadKey();
+
+            if (key.Key == ConsoleKey.Escape)
+                Menu();
+            else if (key.Key != ConsoleKey.Escape)
+            {
+                Console.WriteLine("Нет такой команды! Повторите.");
+                key = Console.ReadKey();
+            }
         }
 
-        private bool OutputToFileTXT()
+        private async void OutputToFileTXT()
         {
-            return true;
+            
+            Console.WriteLine("####  запись в .txt  ####");
+            Console.WriteLine(" ");
+            string writePath = @"C:\GitHub\WPF2021\BodyFlight\test.txt";
+            //string text = "Привет мир!\nПока мир...";
+            Console.WriteLine("Введите интрервал с которым хотите получать координаты полёта тела:");
+            Console.WriteLine("!Запятая!");
+            double diameter = Convert.ToDouble(Console.ReadLine());
+            double flightLength = Math.Round(((speed * speed * Math.Sin(2 * angle * Math.PI / 180)) / G), 3);
+            double newangle = angle * Math.PI / 180;
+            double t;
+            if (diameter <= 0)
+            {
+                Console.WriteLine("Вы ввели '0' или  число < 0 . Зачем? Введите число > 0");
+                diameter = Convert.ToDouble(Console.ReadLine());
+            }
+            else if (diameter > flightLength)
+            {
+                Console.WriteLine("Ошибка число больше чем, максимальной длины полета. Введите число заново.");
+                diameter = Convert.ToDouble(Console.ReadLine());
+            }
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default))
+                {
+                    for (double l = 0; l <= flightLength; l += diameter)
+                    {
+                        t = l / (speed * Math.Cos(newangle));
+                        await sw.WriteLineAsync("[x]:\t"
+                            + Math.Round(l, 3) + '\t' 
+                            + "[y]:\t" 
+                            + Math.Round((speed * t * Math.Sin(newangle) - (G * t * t) / 2), 3));
+                    }
+                }
+                Console.WriteLine("Запись выполнена");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Ошибка");
+                statusTXT = false;
+            }
+
+            statusTXT = true;
+
+            Console.WriteLine(" ");
+            Console.WriteLine("------------------------------------------------------");
+            Console.WriteLine(" ");
+            Console.WriteLine("[Esc] - вернуться назад");
+            ConsoleKeyInfo key = Console.ReadKey();
+
+            if (key.Key == ConsoleKey.Escape)
+                Menu();
+            else if (key.Key != ConsoleKey.Escape)
+            {
+                Console.WriteLine("Нет такой команды! Повторите.");
+                key = Console.ReadKey();
+            }
         }
 
         private void OutputInformation()
@@ -158,11 +263,11 @@ namespace BodyFlight
                 angle);
             Console.WriteLine("[2] - Полное время полета тела:      " +
                 Math.Round(time, 3));
-            Console.WriteLine("[3] - Максимальная высота полета тела:       " +
-                Math.Round((speed * speed * Math.Sin(angle * Math.PI / 180)
-                * Math.Sin(angle * Math.PI / 180)) / 2*G), 3);
-            Console.WriteLine("[4] - Растояние которое пролетело тело:      " +
+            Console.WriteLine("[3] - Растояние которое пролетело тело:      " +
                 Math.Round((speed * speed * Math.Sin(2 * angle * Math.PI / 180)) / G), 3);
+            //Console.WriteLine("[4] - Максимальная высота полета тела:       " +
+            //   Math.Round((speed * speed * Math.Sin(angle * Math.PI / 180)
+            //   * Math.Sin(angle * Math.PI / 180)) / 2 * G), 3);
             Console.WriteLine(" ");
             Console.WriteLine("------------------------------------------------------");
             Console.WriteLine(" ");
@@ -185,17 +290,30 @@ namespace BodyFlight
             Console.WriteLine("  ##########  Координаты полета  #########");
             Console.WriteLine(" ");
 
-            Console.WriteLine("Введите интрервал времени с которым хотите получать координаты полёта тела:");
-            Console.WriteLine("!Вместо запятых нужно узаказывать точку!");
-            double intervalTime =  Convert.ToDouble(Console.ReadLine());
-            if(intervalTime <= 0)
+            Console.WriteLine("Введите интрервал с которым хотите получать координаты полёта тела:");
+            Console.WriteLine("!Запятая!");
+            double diameter =  Convert.ToDouble(Console.ReadLine());
+            double flightLength = Math.Round(((speed * speed * Math.Sin(2 * angle * Math.PI / 180)) / G), 3);
+            double newangle = angle * Math.PI / 180;
+            double t;
+            if (diameter <= 0)
             {
                 Console.WriteLine("Вы ввели '0' или  число < 0 . Зачем? Введите число > 0");
-                intervalTime = Convert.ToDouble(Console.ReadLine());
+                diameter = Convert.ToDouble(Console.ReadLine());
             }
-            //for (double t = 0; t <= time; t += intervalTime)
-            //    Console.WriteLine("[x]:\t"+ Math.Round((speed * t * Math.Cos(angle * Math.PI / 180)), 3) +'\t' 
-            //        + "[y]:\t" + Math.Round((speed * t * Math.Sin(angle * Math.PI / 180) - G * t * t / 2), 3));
+            else if(diameter > flightLength)
+            {
+                Console.WriteLine("Ошибка число больше чем, максимальной длины полета. Введите число заново.");
+                diameter = Convert.ToDouble(Console.ReadLine());
+            }
+
+            for (double l = 0; l <= flightLength; l += diameter)
+            {
+                t = l / (speed * Math.Cos(newangle));
+                Console.WriteLine("[x]:\t"
+                    + Math.Round(l,3) + '\t' +"[y]:\t" + Math.Round((speed * t * Math.Sin(newangle) - (G * t * t) / 2), 3)); 
+            }
+
             Console.WriteLine(" ");
             Console.WriteLine("------------------------------------------------------");
             Console.WriteLine(" ");
