86f4b04cd58c8d69449c6e7b91045b08ff838056
Jarosloov <jarosloov@yandex.ru>
2021-04-09 16:16:21 +0500

    Добавление метода Input()

--- BallisticFlight/Program.cs
+++ BallisticFlight/Program.cs
@@ -11,80 +11,98 @@ namespace BallisticFlight
         static void Main(string[] args)
         {
 
-
+            
 
         }
     }
 
     class BallisticFlight
     {
+        List<double> coordinates_X = 
+            new List<double>();                             // cлаварь сохранения координат по Х
+        List<double> coordinates_Y =
+            new List<double>();                             // cлаварь сохранения координат по y
+        List<double> velocityProjections_X =
+           new List<double>();                              // словарь сохранения проекций скоростей по y
+        List<double> velocityProjections_Y =
+           new List<double>();                              // словарь сохранения проекций скоростей по Х
+
+        private const double AccelerationOfFreeFall = 9.81; // ускорение свободного падения
+
+        private double startSpeed;                          // начальная скорость
+        private double angleOfInclination;                  // угол наклона
+        private double startHeight;                         // начальная высота
+
+        private double flightTime;                          // время плёта
+        private double deltaFlightTime;                     // разбиение времени 
+
+        private double coefficientResistance;               // коэффицент сопростивления 
+        private double materialDensity;                     // плотность тела
+        private double airDensity;                          // плотность воздуха 
+        private double bodyRadius;                          // радиус 
+
+        private double resistanceForce;                     // сила попротивления
+
+        private double maxFlightLength;                     // максимальная длина полёта
+        private double maxflightAltitude;                   // максимальная высота полёта 
+
+        private bool statusDataEntry = false;               // статус наличия данных
+        private bool statusEntriesInTextDocument = false;   // статус записи в текстовый докусент
+        private bool statusEntriesInExcel = false;          // статус записи в эксель
+
         public BallisticFlight()
         {
             Menu();
         }
 
+        private void Input()
+        {
+            Console.Clear();
+            Console.WriteLine("Введите скорость тела: ");
+            startSpeed = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите угол наклона: ");
+            angleOfInclination = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите начальную высоту: ");
+            startHeight = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите радиус тела ");
+            bodyRadius = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите коэффицент сопротивления: ");
+            coefficientResistance = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите плотность материала: ");
+            materialDensity = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите плотность воздуха: ");
+            airDensity = Convert.ToDouble(Console.ReadLine());
+
+            resistanceForce = coefficientResistance 
+                * (Math.PI * bodyRadius * bodyRadius) * airDensity / (2 * materialDensity 
+                * (4 / 3) * Math.PI * Math.Pow(bodyRadius, 3));
+
+            flightTime = 2 * startSpeed * Math.Sin(angleOfInclination * Math.PI / 180) / AccelerationOfFreeFall;
+
+            statusDataEntry = true;
+            Menu();
+        }
+        
+        private void Calculate()
+        {
+            coordinates_X.Add(0);
+            coordinates_Y.Add(startHeight);
+        }
+
         private void Menu()
         {
-            Console.WriteLine("############  Meню  ############");
-            if (statusInput)
-            {
-                Console.WriteLine("[D] - Ввод данных \t [нет данных]");
-                Console.WriteLine("[I] - информаця \t [нет данных]");
-                if (statusExcel)
-                    Console.WriteLine("[E] - запись в .excel \t [записано][нет данных]");
-                else
-                    Console.WriteLine("[E] - запись в .excel \t [не записано][нет данных]");
-                if (statusTXT)
-                    Console.WriteLine("[T] - запись в .txt  \t [записано][нет данных]");
-                else
-                    Console.WriteLine("[T] - запись в .txt  \t [не записано][нет данных]");
-                Console.WriteLine("[Esc] - выход");
-            }
-            else
-            {
-                Console.WriteLine("[D] - Ввод новых данных \t [есть данные]");
-                Console.WriteLine("[I] - информаця \t [есть данные]");
 
-                if (statusExcel)
-                    Console.WriteLine("[E] - запись в .excel \t [записано]");
-                else
-                    Console.WriteLine("[E] - запись в .excel \t [не записано]");
-                if (statusTXT)
-                    Console.WriteLine("[T] - запись в .txt  \t [записано]");
-                else
-                    Console.WriteLine("[T] - запись в .txt  \t [не записано]");
-                Console.WriteLine("[Esc] - выход");
-            }
+        }
 
-            ConsoleKeyInfo key = Console.ReadKey();
-            while (true)
-            {
-                if (key.Key == ConsoleKey.I)
-                    OutputInformation();
-                else if (key.Key == ConsoleKey.E)
-                {
-                    OutputToFileExcel();
-                    Console.Clear();
-                }
-                else if (key.Key == ConsoleKey.T)
-                {
-                    OutputToFileTXT();
-                    Console.Clear();
-                }
-                else if (key.Key == ConsoleKey.D)
-                {
-                    Input();
-                    Console.Clear();
-                }
-                else if (key.Key == ConsoleKey.Escape)
-                    Process.GetCurrentProcess().Kill();
-                else if ((key.Key != ConsoleKey.I) || (key.Key != ConsoleKey.D) || (key.Key != ConsoleKey.E) ||
-                    (key.Key != ConsoleKey.T) || (key.Key != ConsoleKey.Escape))
-                {
-                    Console.WriteLine("Нет такой команды! Повторите.");
-                    key = Console.ReadKey();
-                }
-            }
+        private void Menu(int test)
+        {
+
         }
 
         private void GoBack(string SpecifyingLocation)
@@ -98,11 +116,10 @@ namespace BallisticFlight
             {
                 if (SpecifyingLocation == "Menu")
                 {
-                    Menu();
+                    //Menu();
                 }
-                else
                     Menu();
-            } 
+            }
             else if (key.Key != ConsoleKey.Escape)
             {
                 Console.WriteLine("Нет такой команды! Повторите.");
